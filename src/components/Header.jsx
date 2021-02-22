ae201d83342cf31e4a0ca8d9298fb2955b230e4b
alicp <alicruzaguila@gmail.com>
2021-02-22 08:40:01 -0600

    construyo platziVideo login agregado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import gravatar from '../utils/gravatar';
+
 import '../assets/styles/components/Header.scss';
 
 import logo from '../assets/static/logo-platzi-video-BW2.png';
@@ -9,27 +12,41 @@ const profile = 'Perfil';
 const account = 'Cuenta';
 const logIn = 'Iniciar Sesión';
 
-const Header = () => (
-	<header className='header'>
-		<Link to='/'>
-			<img className='header__img' src={logo} alt='Platzi Video' />
-		</Link>
+const Header = (props) => {
+	const { user } = props;
+	const hasUser = Object.keys(user).length > 0;
+	return (
+		<header className='header'>
+			<Link to='/'>
+				<img className='header__img' src={logo} alt='Platzi Video' />
+			</Link>
 
-		<div className='header__menu'>
-			<div className='header__menu--profile'>
-				<img src={userIcon} alt='user icon' />
-				<p>{profile}</p>
+			<div className='header__menu'>
+				<div className='header__menu--profile'>
+					{hasUser ? (
+						<img src={gravatar(user.email)} alt={user.email} />
+					) : (
+						<img src={userIcon} alt='user icon' />
+					)}
+					<p>{profile}</p>
+				</div>
+				<ul>
+					<li>
+						<Link to='Register'>{account}</Link>
+					</li>
+					<li>
+						<Link to='Login'>{logIn}</Link>
+					</li>
+				</ul>
 			</div>
-			<ul>
-				<li>
-					<Link to='Login'>{account}</Link>
-				</li>
-				<li>
-					<Link to='Register'>{logIn}</Link>
-				</li>
-			</ul>
-		</div>
-	</header>
-);
+		</header>
+	);
+};
 
-export default Header;
+const mapStateToProps = (state) => {
+	return {
+		user: state.user,
+	};
+};
+
+export default connect(mapStateToProps, null)(Header);
